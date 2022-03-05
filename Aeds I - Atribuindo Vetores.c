95fc8eb75ad3d91d422c6e40182793481a87039c
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-03-04 22:26:21 -0300

    refactored code
    Just a suggestion.

--- Aeds I - Atribuindo Vetores.c
+++ Aeds I - Atribuindo Vetores.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
-  int i, j, vet[10], vet2[10];
-
+int i, j, vet[10], vet2[10];
 
 int main(void)
 {
 
   for (i = 0; i < 10; i++)
-    {
-      printf("Escreva os valores no Vetor\n");
-      scanf("%i", &vet[i]);
-     }
-
-    for (i = 0; i < 10; i++)
-      {
+  {
+    printf("Escreva os valores no Vetor\n");
+    scanf("%i", &vet[i]);
     vet2[i] = vet[i];
-      }
-
+    
     printf("\n");
     printf("Valores\n");
-
+    
     for (i = 0; i < 10; i++)
-      {
+    {
+      printf("%i\n", vet2[i]);
+    }
+  }
 
-    printf("%i\n", vet2[i]);
-
-     }
 }
