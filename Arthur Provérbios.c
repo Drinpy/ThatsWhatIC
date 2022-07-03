38fbe20a6a017218e1f5f722be4ecaccea817dc6
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2022-07-02 22:06:03 -0300

    Create Arthur Provérbios.c

---
+++ Arthur Provérbios.c
@@ -0,0 +1,66 @@
+#include <stdio.h>  // Standard input output
+#include <stdlib.h> // Standard library
+
+// Void significa não retorno, retorno vazio.
+void drinpeOi(int a, int b) // VOID de vazio, lembra do mine
+{
+    printf("Drinpe says oi %i %i", a, b);
+}
+
+// Assinatura de retorno inteiro
+int soma(int a, int b)
+{
+    return 1 + 2;
+}
+
+// Diferença entre aspas simples e aspas duplas
+char nome = "Drinpe"; // Aspas duplas -> Letras, lista de LETRAS
+char letra = 'D';      // Aspas simples -> LETRA, singular, sozinha, alone.
+
+void trabalhoLuizinho(inta, int b){
+    a = b * 100;
+}
+
+// ponteiro de char = 0x1 .. 0x2 .. 0x5 .. 0x6
+
+int main()
+{
+    // printf("%c %c %c %c %c %c", nome[0], nome[1], nome[2], nome[3], nome[4], nome[5]);
+    // printf("%s", nome);
+
+
+    // int a = 24, b = 69;
+    // drinpeOi(a, b);
+    // printf("\n%i ", soma(a, b));
+
+    // int numero = 777;
+    // intponteiro = &numero;
+
+    // int ponteiro[10];
+
+    // int numero2 = 888;
+    // numero = 10;
+    // ponteiro = &numero2;
+
+    // printf("Numero -> %i Ponteiro -> %i", numero, ponteiro);
+
+    // // Me dê 10 espaços na memória
+    // ponteiro = malloc(sizeof(int) 10);
+    // // 0x1 .. 0x2 .. 0x3 .. 0x9 .. 0x10
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     ponteiro[i] = i2;
+    // }
+
+//    printf("\n\n\n [%i] ", ponteiro[9]);
+
+    // intponteiro;
+    // int valor = 10;
+    // ponteiro = &valor;
+
+    // printf("\n %i", ponteiro);
+    // printf("\n %i", *ponteiro);
+
+
+    return 0;
+}
